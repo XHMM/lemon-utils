2bee340d58aac04fda32009af95307ca5440a2f4
xhmm <super.xhmm@gmail.com>
2019-10-22 21:36:15 +0800

    forget to build ts files

--- src/index.test.ts
+++ src/index.test.ts
@@ -10,11 +10,7 @@ describe('utils function test', () => {
     expect(objectToQS(obj)).toBe('id=1&number=12&message=%E4%BD%A0%E5%A5%BD')
   });
   it('convert object containing no-primitive value to querystring', () => {
-    const obj = {
-      id: undefined,
-      number: 12,
-      message: '你好'
-    }
-    expect(objectToQS(obj)).toBe('number=12&message=%E4%BD%A0%E5%A5%BD')
+    const obj = { userNumber: 1326099664, groupNumber: undefined }
+    expect(objectToQS(obj)).toBe('userNumber=1326099664')
   });
 })
